cc34c06d35e27d7845b73b07df174b4d034b2874
Scott Powers <saberpowers@gmail.com>
2025-04-10 15:32:46 -0500

    allow buffer row in write_latex_table

--- R/write_latex_table.R
+++ R/write_latex_table.R
@@ -6,11 +6,23 @@
 #' 
 #' @param table a dataframe to be converted to a L
 #' @param file a file name (presumably ending in .tex) to which to write the LaTeX table
+#' @param buffer_row row number before which to insert blank row
 #' @param include.rownames If `TRUE` the rows names are printed. Default value is `FALSE`.
 #' @param ... additional arugments to `print.xtable`
 #' 
 #' @export
-write_latex_table <- function(table, file, include.rownames = FALSE, ...) {
+write_latex_table <- function(table, file, buffer_row = NULL, include.rownames = FALSE, ...) {
+
+  if (!is.null(buffer_row)) {
+    table_top <- dplyr::slice(table, 1:(buffer_row - 1))
+    table_bot <- dplyr::slice(table, buffer_row:dplyr::n())
+    buffer <- table |>
+      dplyr::slice(1) |>
+      dplyr::mutate(dplyr::across(dplyr::everything(), function(x) {NA})) |>
+      tibble::remove_rownames()
+    table <- dplyr::bind_rows(table_top, buffer, table_bot)
+  }
+
   text <- table |>
     xtable::xtable() |>
     print(
