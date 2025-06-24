d3e0ce18bf21a543da0b6e78d5eae4f777f59028
Scott Powers <saberpowers@gmail.com>
2025-06-23 19:28:37 -0700

    make write_latex_table work better

--- R/write_latex_table.R
+++ R/write_latex_table.R
@@ -6,26 +6,57 @@
 #' 
 #' @param table a dataframe to be converted to a L
 #' @param file a file name (presumably ending in .tex) to which to write the LaTeX table
+#' @param prefix_rows optional vector of rows to include before the contents of the table,
+#'   e.g. super-headers, one character string per row. End-of-line character (double backslash)
+#'   will be appended to the end of each row.
+#' @param colnames optional vector of print-friendly column names to overwrite `colnames(table)`
 #' @param buffer_row row number before which to insert blank row
+#' @param align latex string of column aligments (defaults to all c)
+#' @param digits vector of integers, passed to `xtable::xtable`
 #' @param hline.after a vector indicating the rows after which a horizontal line should appear.
-#'   Default value is NULL.
+#'   Default value is 0.
 #' @param include.rownames If `TRUE` the row names are printed. Default value is `FALSE`.
 #' @param include.colnames If `TRUE` the col names are printed. Default value is `FALSE`.
-#' @param only.contents If `TRUE`` only the rows of the table are printed. Default value is `TRUE`.
+#' @param only.contents This argument must be `FALSE`, and the function will err if it is not.
+#'   This is included as an argument only so that the function will stop gracefully if you try to
+#'   set this argument to `TRUE`.
 #' @param sanitize.test.function A function taking a character vector and returning a sanitized one.
 #'   Default value is `identity`.
 #' @param ... additional arugments to `print.xtable`
- 
 #' @export
 write_latex_table <- function(table,
                               file,
+                              colnames = NULL,
+                              prefix_rows = NULL,
                               buffer_row = NULL,
-                              hline.after = NULL,
+                              align = NULL,
+                              digits = NULL,
+                              hline.after = 0,
                               include.rownames = FALSE,
-                              include.colnames = FALSE,
-                              only.contents = TRUE,
+                              include.colnames = TRUE,
+                              include.contents = FALSE,
                               sanitize.text.function = identity,
                               ...) {
+
+
+  if (is.null(colnames)) {
+    colnames <- colnames(table)
+  }
+  if (length(colnames) != ncol(table)) {
+    stop("length(colnames) does not match ncol(table)")
+  }
+  if (include.rownames) {
+    colnames <- c("", colnames)
+  }
+
+  ncol <- ncol(table) + include.rownames
+  if (is.null(align)) {
+    align <- paste(rep("c", times = ncol), collapse = "")
+  }
+
+  if (!only.contents) {
+    stop("`only.contents` must be false")
+  }
 
   if (!is.null(buffer_row)) {
     table_top <- dplyr::slice(table, 1:(buffer_row - 1))
@@ -37,20 +68,36 @@ write_latex_table <- function(table,
     table <- dplyr::bind_rows(table_top, buffer, table_bot)
   }
 
+  colnames_row <- colnames |>
+    paste(collapse = " & ") |>
+    paste("\\\\")
+  if (!include.colnames) {
+    colnames_row <- NULL
+  }
+
   text <- table |>
-    xtable::xtable() |>
+    xtable::xtable(digits = digits) |>
     print(
       hline.after = hline.after,
       include.rownames = include.rownames,
-      include.colnames = include.colnames,
-      only.contents = only.contents,
+      include.colnames = FALSE,
+      only.contents = TRUE,
       sanitize.text.function = sanitize.text.function,
       ...
     ) |>
     capture.output()
-  text <- text[-length(text)]
-  text[length(text)] <- text[length(text)] |>
-    stringr::str_replace(" $", "") |>
-    stringr::str_replace("\\\\\\\\$", "")
-  write(text, file = file)
+
+  c(
+    paste0("\\begin{tabular}{", align, "}"),
+    paste(prefix_rows, "\\\\"),
+    colnames_row,
+    text[-length(text)],    # final line is always blank
+    "\\end{tabular}"
+  ) |>
+    write(file = file)
+#  text <- text[-length(text)]
+#  text[length(text)] <- text[length(text)] |>
+#    stringr::str_replace(" $", "") |>
+#    stringr::str_replace("\\\\\\\\$", "")
+#  write(text, file = file)
 }
